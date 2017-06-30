ae5856a01f66f279d6202a22708b4cd1df102f1a
Christopher Cartlidge <ccartlidge@opentable.com>
2017-06-30 11:51:46 +0100

    Coverted over to apollo

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const http = require('http');
-const request = require('request');
 const _ = require('lodash');
+const http = require('http');
+const gql = require('graphql-tag');
+const { ApolloClient, createBatchingNetworkInterface } = require('apollo-client');
 
-let settings;
+// Pollyfill: https://github.com/apollographql/apollo-client/issues/1225
+require('isomorphic-fetch'); // eslint-disable-line global-require
 
-module.exports.register = (opts, dependencies, next) => {
+let client;
+
+const mergeHeaderArguments = (options, headers) =>
+  _.merge(options, { variables: { __headers: headers } });
+
+module.exports.register = (opts, dependencies, next) => { // eslint-disable-line consistent-return
+  if (opts.batchInterval && !_.isInteger(opts.batchInterval)) {
+    return next(new Error('The batchInterval parameter is invalid'));
+  }
+
   if (!opts.serverUrl) {
     return next(new Error('The serverUrl parameter is invalid'));
   }
 
-  settings = opts;
-  return next();
+  const options = { uri: opts.serverUrl, batchInterval: opts.batchInterval || 10 };
+  const networkInterface = createBatchingNetworkInterface(options);
+
+  networkInterface.use([{
+    applyMiddleware(req, done) {
+      if (!req.options.headers) {
+        // eslint-disable-next-line
+        req.options.headers = _.extend({ 'User-Agent': 'oc' }, req.request.variables.__headers);
+      }
+      done();
+    },
+  }]);
+
+  client = new ApolloClient({ networkInterface, queryDeduplication: true });
+
+  next();
 };
 
 module.exports.execute = () => ({
-  query: (options, headers, timeout) => new Promise((resolve, reject) => request({
-    body: {
-      query: options.query,
-      variables: options.variables,
-      operationName: null,
-    },
-    headers: _.extend({ 'User-Agent': 'oc' }, headers),
-    json: true,
-    method: 'POST',
-    timeout,
-    url: settings.serverUrl,
-  }, (err, result, body) => {
-    if (err) {
-      return reject(new Error(err));
-    } else if (result.statusCode !== 200) {
-      return reject(new Error(http.STATUS_CODES[result.statusCode]));
-    }
-
-    return resolve(body);
-  })),
+  //See: https://github.com/apollographql/apollo-client/issues/1419
+  query: (query, variables, headers, timeout) => client.networkInterface.query({
+      query: gql(query),
+      variables: mergeHeaderArguments(variables, headers)
+    })
 });
